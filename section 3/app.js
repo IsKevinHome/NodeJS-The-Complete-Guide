25345573303e95036d36c5647a3b818519fe63a6
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2023-04-02 15:43:18 -0700

    assignment 2, app.js updates

--- section 3/app.js
+++ section 3/app.js
@@ -3,12 +3,19 @@ const express = require("express");
 const app = express();
 
 // app.use function runs every request (middleware)
-app.use((req, res, next) => {
-    console.log("Inside the middleware");
-    next(); // Allows the request to continue to the next middleware.
+// app.use((req, res, next) => {
+//     console.log("Inside the middleware");
+//     next(); // Allows the request to continue to the next middleware.
+// });
+
+// Remember middleware runs top to bottom.
+app.use("/add-product", (req, res, next) => {
+    console.log("In another middleware");
+    res.send("<h1>Add Product</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
+    // the '/' here will run anytime that '/' is included
     console.log("Inside the middleware");
     res.send("<h1>Hello from express</h1>");
 });
