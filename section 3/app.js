acd606bdb62ff443e1c9291857946ea86a25a237
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2023-04-02 13:45:07 -0700

    remove htttp and add express

--- section 3/app.js
+++ section 3/app.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const express = require("express");
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+// app.use function runs every request (middleware)
+app.use((req, res, next) => {
+    console.log("Inside the middleware");
+    next(); // Allows the request to continue to the next middleware.
+});
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    console.log("Inside the middleware");
+    res.send("<h1>Hello from express</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
