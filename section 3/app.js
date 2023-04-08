f7ed2cef78d9c83887bc46dd0f6f44d7f94e4645
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2023-04-08 15:02:16 -0700

    add 404 middleware

--- section 3/app.js
+++ section 3/app.js
@@ -11,4 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 Middleware (added to bottom as a catch-all)
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Page not found</h1>");
+});
+
 app.listen(3000);
