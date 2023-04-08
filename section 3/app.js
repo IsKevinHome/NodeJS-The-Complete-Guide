eb61d0f4091f55e34ce83ead6421896e4b56a5bf
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2023-04-08 14:48:37 -0700

    add post instead of use

--- section 3/app.js
+++ section 3/app.js
@@ -3,23 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false })); // parses incoming request body (req.body will be undefined by default)
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Remember middleware runs top to bottom.
 app.use("/add-product", (req, res, next) => {
     res.send(
         '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
     );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-    // the '/' here will run anytime that '/' is included
-    res.send("<h1>Hello from express</h1>");
+    res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
