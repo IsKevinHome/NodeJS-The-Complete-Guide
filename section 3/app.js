404a7f45071d72a2c34db60f0511dfc329f2dfa3
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2023-04-08 15:18:43 -0700

    add common starting segment to add-product in app and routes

--- section 3/app.js
+++ section 3/app.js
@@ -8,11 +8,10 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-// 404 Middleware (added to bottom as a catch-all)
-
+// 404 Middleware / catch all
 app.use((req, res, next) => {
     res.status(404).send("<h1>404 Page not found</h1>");
 });
